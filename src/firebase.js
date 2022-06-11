7613b84a1c9e9f96fb932e8b5279760e42b60cee
Alex Gill <4456591+upalexgill@users.noreply.github.com>
2022-06-11 22:41:59 +0100

    Database expired

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/database";
 
 let config = {
-  apiKey: "AIzaSyAOvKza4oE5mPSg9eHwt6t_jn1cuETYijI",
-  authDomain: "smart-abacus-290523.firebaseapp.com",
-  databaseURL: "https://smart-abacus-290523-default-rtdb.firebaseio.com",
-  projectId: "smart-abacus-290523",
-  storageBucket: "smart-abacus-290523.appspot.com",
-  messagingSenderId: "907441566826",
-  appId: "1:907441566826:web:53dcf983e76088d5f1c613",
+  databaseURL: "https://crud-469c1-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyC8tWxi1rdIEabkCOo7zSVQaLOoWL587Is",
+  authDomain: "crud-469c1.firebaseapp.com",
+  projectId: "crud-469c1",
+  storageBucket: "crud-469c1.appspot.com",
+  messagingSenderId: "690076249772",
+  appId: "1:690076249772:web:64e146a0abc34aafe3b909"
 };
 
 firebase.initializeApp(config);
